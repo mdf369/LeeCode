4b5f05e092d76318695a263f262aaed699f51b09
mdf369 <952945925@qq.com>
2019-04-26 10:31:50 +0800

    update

--- src/Main.java
+++ src/Main.java
@@ -1,60 +1,154 @@
+import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.List;
 import java.util.PriorityQueue;
 import java.util.Queue;
 import java.util.Scanner;
+import java.util.concurrent.Callable;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.FutureTask;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 public class Main {
 
-  public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
+  private static final int FIELD_NUM = 7;
+  private static final int[] USEFUL_FIELDS = new int[]{0, 6, 5, 3};
+  private static final char SPLITTER = ',';
+  private static final char QUOTE = '"';
+  private static final String DOUBLE_QUOTES = "\"\"";
 
-    while (sc.hasNext()) {
-      int n = sc.nextInt();
+  private static final String BIO_NOTES_FORMAT = "%s, %s years old, is from %s and is interested in %s.";
 
-      String[] words = new String[n];
-      for (int i = 0; i < n; i++) {
-        words[i] = sc.next();
-      }
-      String T = sc.next();
+  private static ExecutorService fixedThreadPool = Executors.newCachedThreadPool();
 
-      if (n < 1) {
-        System.out.println(0);
-        continue;
-      }
+  static class SimplifyWorker implements Callable<String> {
 
-      Queue<int[]> candidates = new PriorityQueue<>(new Comparator<int[]>() {
-        @Override
-        public int compare(int[] o1, int[] o2) {
-          return o1[1] - o2[1];
-        }
-      });
-      for (int i = 0; i < words.length; i++) {
-        updateCandidates(candidates, T, 0, words[i], new int[]{0, 0, i});
+    private String field;
+
+    public SimplifyWorker(String field) {
+      this.field = field;
+    }
+
+    private String simplify(String field) throws InterruptedException {
+      if (field.length() == 0) {
+        return field;
       }
 
-      int count = 0;
-      int TIndex = 0;
-      while (!candidates.isEmpty()) {
-        int[] candidate = candidates.poll();
-        if (candidate[0] < TIndex) {
-          updateCandidates(candidates, T, TIndex, words[candidate[2]], candidate);
-          continue;
-        }
-        TIndex = candidate[1];
-        count++;
-
-        updateCandidates(candidates, T, TIndex, words[candidate[2]], candidate);
+      if (field.charAt(0) == QUOTE && field.charAt(field.length() - 1) == QUOTE) {
+        field = field.substring(1, field.length() - 1);
       }
-      System.out.println(count);
+
+      Thread.sleep(100);
+      return field.replace(DOUBLE_QUOTES, "" + QUOTE);
+    }
+
+    @Override
+    public String call() throws Exception {
+      return simplify(field);
     }
   }
 
-  private static void updateCandidates(Queue<int[]> candidates, String T, int TIndex, String word, int[] oldCandidate) {
-    int index = T.indexOf(word, TIndex);
-    if (index != -1) {
-      oldCandidate[0] = index;
-      oldCandidate[1] = index + word.length();
-      candidates.add(oldCandidate);
+//  public static void main(String[] args) {
+//    Scanner sc = new Scanner(System.in);
+//
+//    while (sc.hasNext()) {
+//      String line = sc.nextLine();
+//      outputBioNote(line);
+//    }
+//  }
+
+  private static void outputBioNote(String line) throws ExecutionException, InterruptedException {
+    String[] fields = split(line);
+
+    fixedThreadPool = Executors.newFixedThreadPool(USEFUL_FIELDS.length);
+    Future<String>[] workers = new Future[USEFUL_FIELDS.length];
+    for (int i = 0; i < USEFUL_FIELDS.length; i++) {
+      int index = USEFUL_FIELDS[i];
+      workers[i] = fixedThreadPool.submit(new SimplifyWorker(fields[index]));
     }
+    fixedThreadPool.shutdown();
+    for (int i = 0; i < workers.length; i++) {
+      int index = USEFUL_FIELDS[i];
+      fields[index] = workers[i].get();
+    }
+
+//    for (int i = 0; i < USEFUL_FIELDS.length; i++) {
+//      int index = USEFUL_FIELDS[i];
+//      fields[index] = simplify(fields[index]);
+//    }
+
+    System.out.println(buildBioNote(fields));
+  }
+
+  private static String simplify(String field) throws InterruptedException {
+    if (field.length() == 0) {
+      return field;
+    }
+
+    if (field.charAt(0) == QUOTE && field.charAt(field.length() - 1) == QUOTE) {
+      field = field.substring(1, field.length() - 1);
+    }
+
+    Thread.sleep(100);
+    return field.replace(DOUBLE_QUOTES, "" + QUOTE);
+  }
+
+  private static String buildBioNote(String[] fields) {
+    return String.format(BIO_NOTES_FORMAT, fields[0], fields[6], fields[5], fields[3]);
+  }
+
+  private static String[] split(String line) {
+    String[] fields = new String[FIELD_NUM];
+    int index = 0;
+    int pre = 0;
+    for (int i = 0; i < line.length(); i++) {
+      if (line.charAt(i) == SPLITTER) {
+        fields[index++] = line.substring(pre, i);
+        pre = i + 1;
+      }
+      if (line.charAt(i) == QUOTE) {
+        i = findRightQuote(line, i + 1);
+      }
+    }
+    fields[index] = line.substring(pre);
+    return fields;
+  }
+
+  private static int findRightQuote(String line, int start) {
+    while (start < line.length()) {
+      start = line.indexOf(QUOTE, start);
+      if (isDoubleQuotes(line, start)) {
+        start = findDoubleQuotes(line, start + DOUBLE_QUOTES.length()) + DOUBLE_QUOTES.length();
+      } else {
+        return start;
+      }
+    }
+    return -1;
+  }
+
+  private static int findDoubleQuotes(String line, int start) {
+    return line.indexOf(DOUBLE_QUOTES, start);
+  }
+
+  private static boolean isDoubleQuotes(String line, int index) {
+    index++;
+    if (index == line.length()) {
+      return false;
+    }
+    return line.charAt(index) == QUOTE;
+  }
+
+  public static void main(String[] args) throws ExecutionException, InterruptedException {
+    String line = "\"Dongfang \"\"Mao\"\"\",Shark,952945925@qq.com,\"basketball, fitness\",666,Chengdu,24";
+    long time = System.currentTimeMillis();
+    for (int i = 0; i < 100; i++) {
+      outputBioNote(line);
+    }
+    System.out.println(System.currentTimeMillis() - time);
   }
 }
--- src/top_interview/Temp.java
+++ src/top_interview/Temp.java
@@ -7,15 +7,16 @@ import java.util.List;
 public class Temp {
 
   public static void main(String[] args) throws Exception {
-    ScrambleString test = new ScrambleString();
-    String s = "abc";
-    String p = "bca";
-    int[] num1 = {0,0,1,1,1,1,2,3,3};
+    InterleavingString test = new InterleavingString();
+    String s = "aabcc";
+    String p = "dbbca";
+    String s3 = "aadbbbaccc";
+    int[] num1 = {1,2,3,4,5};
     int[] num2 = {3, 4, 7, 8, 14};
     char[] chars = {'A','A','A','B','B','B'};
     String[] strs = new String[]{"This", "is", "an", "example", "of", "text", "justification."};
-    int n = 4;
-    int m = 2;
+    int n = 5;
+    int m = 5;
     int[][] matrix = new int[][]{{-8},{-5},{-4},{-2},{0},{3},{4},{5},{6},{8},{9},{11},{13},{16},{18},{20},{23},{25},{26},{27},{29},{31},{32},{34},{37},{38},{41},{44},{46},{49},{51},{53},{56},{57},{58},{60},{63},{65},{68},{70},{73},{76},{77},{79},{82},{84},{87},{90},{92},{94}};
     char[][] board = new char[][]{
         {'1', '0', '1', '0', '0'},
@@ -24,7 +25,7 @@ public class Temp {
         {'1', '0', '0', '1', '0'}};
     List<String> wordList = Arrays.asList("hot","dot","dog","lot","log");
 
-    System.out.println(test.isScramble(s, p));
+    System.out.println(test.isInterleave(s, p, s3));
 //      Tool.display(test.combine(n, m));
 //        Tool.display(num1);
 //        test.nextPermutation(num1);
